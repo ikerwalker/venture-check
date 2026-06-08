3d3799d64da90e494b1bb9e1f943690660388f3c
iker walker <ikerwalker@MacBook-Air-de-iker-9.local>
2026-06-07 23:34:21 -0600

    update author name

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t border-gray-200 px-8 py-4 text-center text-sm text-gray-400">
-      Built by Antonio T. · 2026
+      Built by Iker Walker · 2026
     </footer>
   );
 }
